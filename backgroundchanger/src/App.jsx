9d94c4e0f18ce8e8f491ab619d41f93d78c8ff19
Anurag Sinha <anurag3487@gmail.com>
2024-01-14 23:52:20 +0530

    Bg Color Changer project

---
+++ backgroundchanger/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("white");
+  const colors = ["red", "blue", "green", "black", "teal"];
+
+  const buttons = colors.map((color) => {
+    return (
+      <button
+        onClick={() => setColor(color)}
+        className="px-4 py-3 rounded-3xl shadow-xl"
+        style={{ backgroundColor: color, color: "white" }}
+      >
+        {color}
+      </button>
+    );
+  });
+  return (
+    <>
+      <div style={{ backgroundColor: color, width: "100%", height: "100vh" }}>
+        <h1 style={{ textAlign: "center", color: "white" }}>
+          BG Color Changer
+        </h1>
+        <div
+          style={{
+            marginTop: "400px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div>{buttons}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
