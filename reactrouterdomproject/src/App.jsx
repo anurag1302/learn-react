892a7e134867785d51e5bd9c20fd9e547d42e4e4
Anurag Sinha <anurag3487@gmail.com>
2024-01-25 00:00:20 +0530

    tailwind responsive grid

--- reactrouterdomproject/src/App.jsx
+++ reactrouterdomproject/src/App.jsx
@@ -1,7 +1,16 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+        <div className="bg-red-500">Div1</div>
+        <div className="bg-green-500">Div2</div>
+
+        <div className="bg-blue-500">Div3</div>
+        <div className="bg-teal-500">Div4</div>
+        <div className="bg-yellow-500">Div5</div>
+        <div className="bg-lime-500">Div6</div>
+      </div>
     </>
   );
 }
